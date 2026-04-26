3d9e1f1bd2e1ce27e16a60005296a8c657e72a8e
kaushal2910 <sonawanekaushal05@gmail.com>
2026-04-26 20:33:24 +0530

    env check

--- src/firebase.js
+++ src/firebase.js
@@ -18,6 +18,13 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+
+console.log("FIREBASE CONFIG CHECK:", {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+});
+
+
 const app = initializeApp(firebaseConfig);  
 const db = getFirestore(app);
 const auth = getAuth(app);
